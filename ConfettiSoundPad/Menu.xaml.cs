1608c710f40a6e658dc6cdd7920b38dc35e3fe1b
Eyal Gal <170107eyalgal@gmail.com>
2024-07-15 09:36:02 +0300

    Adding the option to record a file and play it through speakers only

--- ConfettiSoundPad/Menu.xaml.cs
+++ ConfettiSoundPad/Menu.xaml.cs
@@ -1,37 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using NAudio.Wave;
 
 namespace ConfettiSoundPad
 {
-    /// <summary>
-    /// Interaction logic for Menu.xaml
-    /// </summary>
     public partial class Menu : Page
     {
-        private System.Media.SoundPlayer player;
+        // record sound
+        private WaveInEvent waveIn;
+        private WaveFileWriter waveFile;
+
+        // play sound
+        private WaveOutEvent waveOut;
+        private AudioFileReader audioFileReader;
+
+        private string filePath = @"C:\Users\17010\Desktop\sound.wav";
 
         public Menu()
         {
             InitializeComponent();
 
-            this.player = new System.Media.SoundPlayer(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\sounds\sound.wav");
+            // prepare to start recording
+            waveIn = new WaveInEvent();
+            waveIn.WaveFormat = new WaveFormat(44100, 1);
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.RecordingStopped += OnRecordingStopped;
+
+            // prepare to play sound
+            waveOut = new WaveOutEvent();
+            
         }
 
         private void PlaySound_Click(object sender, RoutedEventArgs e)
         {
-            this.player.Play();
+            if (audioFileReader == null)
+            {
+                audioFileReader = new AudioFileReader(filePath);
+                waveOut.Init(audioFileReader);
+            }
+            waveOut.Play();
+        }
+
+        private void RecordSound_Click(object sender, RoutedEventArgs e)
+        {
+            if (Record.Background == Brushes.LightGray) // start recording
+            {
+                waveFile = new WaveFileWriter(filePath, waveIn.WaveFormat); // prepare for recording
+
+                waveIn.StartRecording();
+                Record.Content = "Stop Recording";
+                Record.Background = Brushes.Red;
+            }
+            else // stop recording
+            {
+                waveIn.StopRecording();
+                Record.Content = "Start Recording";
+                Record.Background = Brushes.LightGray;
+            }
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+
+        private void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            // cleare the wave file
+            if (waveFile != null)
+            {
+                waveFile.Dispose();
+                waveFile = null;
+            }
+
+            if (e.Exception != null)
+            {
+                MessageBox.Show($"An error occurred while recording: {e.Exception.Message}");
+            }
         }
     }
 }
